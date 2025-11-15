4cb22d81b6e89aa546bab75a93ec801025a51eeb
mateuszklima <matik.7.11@gmail.com>
2025-11-15 20:03:37 +0100

    4.1.py

--- 4. Formatowanie wyjściowe/4.1.py
+++ 4. Formatowanie wyjściowe/4.1.py
@@ -1,6 +1,6 @@
 name = "Mateusz Klima"
-old = int('20')
-height = "1.84"
+old = 20
+height = 187
 older = old + 6
 print(f"My name is {name}. I am {old}, and my height is {height} cm. In 6 years I will be {older} years old.")
 
